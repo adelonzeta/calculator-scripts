05eb477b41fcb89932a2198b4a0f19c9387aeed4
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2018-12-31 05:47:27 -0800

    text edit

--- src/basic/decimal/decimal.test.ts
+++ src/basic/decimal/decimal.test.ts
@@ -2,21 +2,21 @@ import {decimal} from './decimal'
 import {state} from '../../utilities'
 
 describe('decimal function', () => {
-  describe('when operand is an empty string', () => {
+  describe('operand is an empty string', () => {
     it('should append zero and decimal point', () => {
       expect(decimal(state('', '', ''))).toEqual(state('0.', '', ''))
       expect(decimal(state('1', '', 'ADD'))).toEqual(state('1', '0.', 'ADD'))
     });
   });
 
-  describe('when operand is a whole number', () => {
+  describe('operand is a whole number', () => {
     it('should append a decimal point', () => {
       expect(decimal(state('1', '', ''))).toEqual(state('1.', '', ''))
       expect(decimal(state('1', '1', 'ADD'))).toEqual(state('1', '1.', 'ADD'))
     });
   });
   
-  describe('when operand has a decimal point', () => {
+  describe('operand has a decimal point', () => {
     it('should return the same state', () => {
       expect(decimal(state('1.0', '', ''))).toEqual(state('1.0', '', ''))
       expect(decimal(state('1', '1.0', 'ADD'))).toEqual(state('1', '1.0', 'ADD'))

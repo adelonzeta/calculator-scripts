651fc0b4fa70c572ee99c081782447181f9a2d5b
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2019-01-02 00:50:53 -0800

    tests and optimizations

--- src/basic/operation/operation.test.ts
+++ src/basic/operation/operation.test.ts
@@ -1,48 +1,42 @@
-import {operation} from './operation'    
-// OPERATIONS => 'ADD' || 'SUBTRACT' || 'MULTIPLY' || 'DIVIDE'
-// it should work without operands => null, null, null || 'OPERATIONS'
-// it should work with only the first operand => 1, null, null || 'OPERATIONS'
-// it should calculate the result -> 1, 1, null || 'OPERATIONS'
+import {operation} from './operation'
+import {state} from '../../utilities'
 
-function state(o1: string, o2: string, o: string) {
-  return {operand1: o1, operand2: o2, operator: o}
-}
+describe('operation function', () => {
+  describe('has no operator', () => {
+    it('should save the operator', () => {
+      expect(operation('ADD', state('1', '', ''))).toEqual(state('1', '', 'ADD'))
+      expect(operation('ADD', state('2', '', 'DIVIDE'))).toEqual(state('2', '', 'ADD'))
+      expect(operation('SUBTRACT', state('3', '', ''))).toEqual(state('3', '', 'SUBTRACT'))
+      expect(operation('SUBTRACT', state('4', '', 'MULTIPLY'))).toEqual(state('4', '', 'SUBTRACT'))
+      expect(operation('MULTIPLY', state('5', '', ''))).toEqual(state('5', '', 'MULTIPLY'))
+      expect(operation('MULTIPLY', state('6', '', 'SUBTRACT'))).toEqual(state('6', '', 'MULTIPLY'))
+      expect(operation('DIVIDE', state('7', '', ''))).toEqual(state('7', '', 'DIVIDE'))
+      expect(operation('DIVIDE', state('8', '', 'ADD'))).toEqual(state('8', '', 'DIVIDE'))
+    });
+  });
 
-it('should return null without operands', () => {
-  expect(operation('ADD', state('', '', ''))).toEqual(state('', '', ''))
-  expect(operation('SUBTRACT', state('', '', ''))).toEqual(state('', '', ''))
-  expect(operation('MULTIPLY', state('', '', ''))).toEqual(state('', '', ''))
-  expect(operation('DIVIDE', state('', '', ''))).toEqual(state('', '', ''))
+  describe('has operands and operator', () => {
+    it('should calculate the result', () => {
+      expect(operation('ADD', state('10', '2', 'DIVIDE'))).toEqual(state('5', '', 'ADD'))
+      expect(operation('SUBTRACT', state('4', '2', 'MULTIPLY'))).toEqual(state('8', '', 'SUBTRACT'))
+      expect(operation('MULTIPLY', state('6', '6', 'SUBTRACT'))).toEqual(state('0', '', 'MULTIPLY'))
+      expect(operation('DIVIDE', state('8', '2', 'ADD'))).toEqual(state('10', '', 'DIVIDE'))
+    });
+    it('should calculate big numbers', () => {
+      expect(operation('DIVIDE', state('999999999999999', '999999999999999', 'ADD'))).toEqual(state('1999999999999998', '', 'DIVIDE'))
+    });
+    it('should calculate decimal numbers', () => {
+      expect(operation('ADD', state('1.8747', '45', 'MULTIPLY'))).toEqual(state('84.3615', '', 'ADD'))
+      expect(operation('ADD', state('1.8747', '4.5', 'MULTIPLY'))).toEqual(state('8.43615', '', 'ADD'))
+    });
+  });
+
+  describe('has no operands', () => {
+    it('should return an empty string', () => {
+      expect(operation('ADD', state('', '', ''))).toEqual(state('', '', ''))
+      expect(operation('SUBTRACT', state('', '', ''))).toEqual(state('', '', ''))
+      expect(operation('MULTIPLY', state('', '', ''))).toEqual(state('', '', ''))
+      expect(operation('DIVIDE', state('', '', ''))).toEqual(state('', '', ''))
+    });  
+  });
 });
-
-it('should work with only the first operand', () => {
-  expect(operation('ADD', state('1', '', ''))).toEqual(state('1', '', 'ADD'))
-  expect(operation('ADD', state('2', '', 'DIVIDE'))).toEqual(state('2', '', 'ADD'))
-  expect(operation('SUBTRACT', state('3', '', ''))).toEqual(state('3', '', 'SUBTRACT'))
-  expect(operation('SUBTRACT', state('4', '', 'MULTIPLY'))).toEqual(state('4', '', 'SUBTRACT'))
-  expect(operation('MULTIPLY', state('5', '', ''))).toEqual(state('5', '', 'MULTIPLY'))
-  expect(operation('MULTIPLY', state('6', '', 'SUBTRACT'))).toEqual(state('6', '', 'MULTIPLY'))
-  expect(operation('DIVIDE', state('7', '', ''))).toEqual(state('7', '', 'DIVIDE'))
-  expect(operation('DIVIDE', state('8', '', 'ADD'))).toEqual(state('8', '', 'DIVIDE'))
-});
-
-it('should calculate the result', () => {
-  expect(operation('ADD', state('10', '2', 'DIVIDE'))).toEqual(state('5', '', 'ADD'))
-  expect(operation('SUBTRACT', state('4', '2', 'MULTIPLY'))).toEqual(state('8', '', 'SUBTRACT'))
-  expect(operation('MULTIPLY', state('6', '6', 'SUBTRACT'))).toEqual(state('0', '', 'MULTIPLY'))
-  expect(operation('DIVIDE', state('8', '2', 'ADD'))).toEqual(state('10', '', 'DIVIDE'))
-});
-
-it('should calculate big numbers', () => {
-  // expect(operation('ADD', state(10, 2, 'DIVIDE'))).toEqual(state(5, null, 'ADD'))
-  // expect(operation('SUBTRACT', state(4, 2, 'MULTIPLY'))).toEqual(state(8, null, 'SUBTRACT'))
-  // expect(operation('MULTIPLY', state(6, 6, 'SUBTRACT'))).toEqual(state(0, null, 'MULTIPLY'))
-  expect(operation('DIVIDE', state('999999999999999', '999999999999999', 'ADD'))).toEqual(state('1999999999999998', '', 'DIVIDE'))
-});
-
-it('should return string values', () => {
-  let op = operation('ADD', state('1', '', 'ADD'))
-  expect(typeof op.operand1).toEqual('string')
-});
-
-// it should calculate decimals
--- src/basic/operation/operation.ts
+++ src/basic/operation/operation.ts
@@ -1,22 +1,20 @@
 import {State} from '../../types'
+import {calculate} from '../../utilities'
 
-export function operation(operator: string, state: State): State {
-  const number1 = Number(state.operand1)
-  const number2 = Number(state.operand2)
-  const hasOperands = state.operand1 && state.operand2
-  const existingOperator = state.operator || operator
-  let result = state.operand1
-  let next = state.operator ? '' : state.operand2
-  let newOperator = state.operand1 ? operator : ''
+export function operation(newOperator: string, state: State): State {
+  const hasOperands    = state.operand1 && state.operand2
+  const activeOperator = state.operator || newOperator
+  let operand1 = state.operand1
+  let operand2 = state.operator ? '' : state.operand2
+  let operator = state.operand1 ? newOperator : ''
 
-  if (hasOperands && (existingOperator === 'DIVIDE')) result = String(number1 / number2)
-  if (hasOperands && (existingOperator === 'MULTIPLY')) result = String(number1 * number2)
-  if (hasOperands && (existingOperator === 'SUBTRACT')) result = String(number1 - number2)
-  if (hasOperands && (existingOperator === 'ADD')) result = String(number1 + number2)
-  
-  return {
-    operand1: result, 
-    operand2: next, 
-    operator: newOperator
+  if (hasOperands) {
+    operand1 = calculate({
+      operand1: state.operand1,
+      operand2: state.operand2,
+      operator: activeOperator
+    })
   }
+  
+  return {operand1, operand2, operator}
 }

6eaf75a06c5989668f4ce255b6e8dd81a76e1220
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2019-01-03 12:43:20 -0800

    fix cognitive complexity issue

--- src/basic/decimal/decimal.ts
+++ src/basic/decimal/decimal.ts
@@ -12,17 +12,16 @@ export function decimal(state: State): State {
   let operand1  = state.operand1
   let operand2  = state.operand2
   let operator  = state.operator
+  let notDecimal = !isDecimal(operand(state))
 
-  if (!isDecimal(operand(state))) {
-    if (operator && operand2)
-      operand2 += '.'
-    if (operator && !operand2)
-      operand2 = '0' + '.'
-    if (!operator && operand1)
-      operand1 += '.'
-    if (!operator && !operand1)
-      operand1 = '0' + '.'
-  }
+  if (operator && operand2 && notDecimal)
+    operand2 += '.'
+  if (operator && !operand2 && notDecimal)
+    operand2 = '0' + '.'
+  if (!operator && operand1 && notDecimal)
+    operand1 += '.'
+  if (!operator && !operand1 && notDecimal)
+    operand1 = '0' + '.'
 
   return {operand1, operand2, operator}
 }

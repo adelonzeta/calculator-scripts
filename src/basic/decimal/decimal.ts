6bfb0c45738ed830c76aa3c9809804c4105a47d8
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2019-01-03 12:25:29 -0800

    refactor if statements

--- src/basic/decimal/decimal.ts
+++ src/basic/decimal/decimal.ts
@@ -1,12 +1,21 @@
 import {State} from '../../types'
 
+function operand(state: State): string {
+  return state.operator ? state.operand2 : state.operand1
+}
+
+function isDecimal(operand: string): boolean {
+  return operand.indexOf('.') !== -1
+}
+
 export function decimal(state: State): State {  
   let operand1  = state.operand1
   let operand2  = state.operand2
   let operator  = state.operator
   const period  = '.'
-  const operand = operator ? operand2 : operand1
-  const decimal = operand.indexOf(period) !== -1
+  // const operand = operator ? operand2 : operand1
+  // const decimal = operand(state).indexOf(period) !== -1
+  const decimal= isDecimal(operand(state))
 
   if (operator && operand2 && !decimal)
     operand2 += period
